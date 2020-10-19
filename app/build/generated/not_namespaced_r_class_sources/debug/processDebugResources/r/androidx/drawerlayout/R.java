8abfaebe98f40810618cb6f8ea75c7c47759f79b
Dwi Nur Cahyo <47982352+dwinuray@users.noreply.github.com>
2020-10-19 11:51:10 +0700

    Update

--- app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/drawerlayout/R.java
+++ app/build/generated/not_namespaced_r_class_sources/debug/processDebugResources/r/androidx/drawerlayout/R.java
@@ -86,26 +86,26 @@ public final class R {
         public static final int async = 0x7f080046;
         public static final int blocking = 0x7f08004e;
         public static final int chronometer = 0x7f080059;
-        public static final int forever = 0x7f08007c;
-        public static final int icon = 0x7f080086;
-        public static final int icon_group = 0x7f080087;
-        public static final int info = 0x7f08008c;
-        public static final int italic = 0x7f08008e;
-        public static final int line1 = 0x7f080093;
-        public static final int line3 = 0x7f080094;
-        public static final int normal = 0x7f08009e;
-        public static final int notification_background = 0x7f08009f;
-        public static final int notification_main_column = 0x7f0800a0;
-        public static final int notification_main_column_container = 0x7f0800a1;
-        public static final int right_icon = 0x7f0800b9;
-        public static final int right_side = 0x7f0800ba;
-        public static final int tag_transition_group = 0x7f0800e8;
-        public static final int tag_unhandled_key_event_manager = 0x7f0800e9;
-        public static final int tag_unhandled_key_listeners = 0x7f0800ea;
-        public static final int text = 0x7f0800eb;
-        public static final int text2 = 0x7f0800ec;
-        public static final int time = 0x7f0800ef;
-        public static final int title = 0x7f0800f0;
+        public static final int forever = 0x7f08007b;
+        public static final int icon = 0x7f080085;
+        public static final int icon_group = 0x7f080086;
+        public static final int info = 0x7f08008b;
+        public static final int italic = 0x7f08008d;
+        public static final int line1 = 0x7f080092;
+        public static final int line3 = 0x7f080093;
+        public static final int normal = 0x7f08009d;
+        public static final int notification_background = 0x7f08009e;
+        public static final int notification_main_column = 0x7f08009f;
+        public static final int notification_main_column_container = 0x7f0800a0;
+        public static final int right_icon = 0x7f0800b6;
+        public static final int right_side = 0x7f0800b7;
+        public static final int tag_transition_group = 0x7f0800e5;
+        public static final int tag_unhandled_key_event_manager = 0x7f0800e6;
+        public static final int tag_unhandled_key_listeners = 0x7f0800e7;
+        public static final int text = 0x7f0800e8;
+        public static final int text2 = 0x7f0800e9;
+        public static final int time = 0x7f0800ec;
+        public static final int title = 0x7f0800ed;
     }
     public static final class integer {
         private integer() {}
